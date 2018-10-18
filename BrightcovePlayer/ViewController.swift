d3c3d8570c472b66c080ace9707e5fdf924024ce
Alexey Faizullov <alex.fuzza@gmail.com>
2018-10-18 14:44:50 +0300

    Initial Commit

---
+++ BrightcovePlayer/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  BrightcovePlayer
+//
+//  Created by Alex Faizullov on 10/18/18.
+//  Copyright © 2018 Alex Faizullov. All rights reserved.
+//
+
+import UIKit
+import ZappPlugins
+import ApplicasterSDK
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+    }
+
+    
+    @IBAction func buttonPlay_clicked() {
+        self.presentPlayer()
+    }
+    
+    func presentPlayer() {
+        let item:ZPPlayable = APURLPlayable(streamURL: "https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4", name: "Test Video", description: "")
+        let pluggablePlayer = ZPPlayerManager.sharedInstance.create(playableItem: item)
+        pluggablePlayer.presentPlayerFullScreen?(self, configuration: nil) {
+            pluggablePlayer.pluggablePlayerPlay(nil)
+        }
+    }
+}
+
