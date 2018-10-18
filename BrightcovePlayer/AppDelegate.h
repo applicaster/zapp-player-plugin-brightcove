3ef4a98795b5e9cebf793a353b1665140192546b
Alexey Faizullov <alex.fuzza@gmail.com>
2018-10-18 14:44:50 +0300

    Initial Commit

---
+++ BrightcovePlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BrightcovePlayer
+//
+//  Created by Alex Faizullov on 10/18/18.
+//  Copyright © 2018 Alex Faizullov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
