5b497ebe801850cf3c2500a3c0e5558d5a73245b
Alexey Faizullov <alex.fuzza@gmail.com>
2018-11-06 16:25:43 +0200

    [iOS] Add logs

--- iOS/BrightcovePlayer/AppDelegate.m
+++ iOS/BrightcovePlayer/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+@import ApplicasterSDK;
 
 @interface AppDelegate ()
 
@@ -14,9 +15,8 @@
 
 @implementation AppDelegate
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    [[APLogger sharedInstance] startLogger];
     return YES;
 }
 
