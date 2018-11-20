b607a7f2f4b5f9a4e1b458279ef4bb3f5cb51f6a
Aleksey Malevaniy <a.malevaniy@gmail.com>
2018-11-20 12:32:06 +0200

    [android] Adopt fullscreen mode to youtube-like behaviour

--- Android/brightcove-player-plugin/src/main/java/com/applicaster/player/plugins/brightcove/BrightcovePlayerActivity.kt
+++ Android/brightcove-player-plugin/src/main/java/com/applicaster/player/plugins/brightcove/BrightcovePlayerActivity.kt
@@ -1,13 +1,22 @@
 package com.applicaster.player.plugins.brightcove
 
+import android.app.Activity
+import android.arch.lifecycle.Lifecycle
+import android.arch.lifecycle.LifecycleObserver
+import android.arch.lifecycle.LifecycleOwner
+import android.arch.lifecycle.OnLifecycleEvent
+import android.content.Context
+import android.content.pm.ActivityInfo
+import android.content.res.Configuration
 import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
-import android.view.Window
+import android.view.OrientationEventListener
+import android.view.Surface
 import android.view.WindowManager
 import com.applicaster.player.plugins.brightcove.AnalyticsAdapter.PlayerMode.FULLSCREEN
 import com.applicaster.plugin_manager.playersmanager.Playable
+import com.brightcove.player.event.EventListener
 import com.brightcove.player.event.EventType
 import com.brightcove.player.view.BrightcoveVideoView
 
@@ -15,21 +24,20 @@ class BrightcovePlayerActivity : AppCompatActivity() {
 
   private lateinit var playable: Playable
   private lateinit var videoView: BrightcoveVideoView
+  //
+  private lateinit var orientationController: VideoOrientationController<AppCompatActivity>
+  //
   private lateinit var analyticsAdapter: AnalyticsAdapter
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
-    // setup window for fullscreen
-    requestWindowFeature(Window.FEATURE_NO_TITLE)
-    window.setFlags(
-      WindowManager.LayoutParams.FLAG_FULLSCREEN,
-      WindowManager.LayoutParams.FLAG_FULLSCREEN
-    )
     // inject layout
     setContentView(R.layout.activity_brightcove_player)
     videoView = findViewById(R.id.video_view)
     videoView.eventEmitter.on(EventType.COMPLETED) { finish() }
+
+    orientationController = VideoOrientationController(this, videoView)
 
     // initialize playable
     playable = intent.extras!!.getSerializable(BrightcovePlayerAdapter.KEY_PLAYABLE) as Playable
@@ -38,6 +46,11 @@ class BrightcovePlayerActivity : AppCompatActivity() {
     // initialize tools
     analyticsAdapter = MorpheusAnalyticsAdapter(videoView)
     analyticsAdapter.startTrack(playable, FULLSCREEN)
+  }
+
+  override fun onConfigurationChanged(newConfig: Configuration?) {
+    super.onConfigurationChanged(newConfig)
+    newConfig?.orientation?.also { orientationController.onNewConfigOrientation(it) }
   }
 
   override fun onStart() {
@@ -50,16 +63,92 @@ class BrightcovePlayerActivity : AppCompatActivity() {
     analyticsAdapter.endTrack(playable, FULLSCREEN)
   }
 
-  override fun onWindowFocusChanged(hasFocus: Boolean) {
-    super.onWindowFocusChanged(hasFocus)
-    if (hasFocus) {
-      this.window
-        .decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-          or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-          or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-          or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-          or View.SYSTEM_UI_FLAG_FULLSCREEN
-          or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+}
+
+/**
+ * FullScreen & Orientation rotation controller to sync video view, window params and screen rotation.
+ */
+private class VideoOrientationController<T>(
+  private val activity: T,
+  private val videoView: BrightcoveVideoView,
+  private val sensorEpsilon: Int = 10
+) : OrientationEventListener(activity), LifecycleObserver
+    where T : Activity, T : LifecycleOwner {
+
+  private var fullscreenListenerEnabled = true
+  private var fullscreenTriggered = false
+  private var sensorEnabled = false
+
+  init {
+    initFullScreenClickListener()
+    activity.lifecycle.addObserver(this)
+  }
+
+  @OnLifecycleEvent(Lifecycle.Event.ON_START)
+  fun restore() {
+    if (sensorEnabled) enable()
+  }
+
+  @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+  fun dispose() {
+    disable()
+  }
+
+  private fun initFullScreenClickListener() {
+    val fullScreenListener = EventListener {
+      if (fullscreenListenerEnabled) {
+        fullscreenTriggered = true
+        setOppositeOrientation()
+      } else {
+        fullscreenListenerEnabled = true
+      }
+    }
+    videoView.eventEmitter.on(EventType.DID_ENTER_FULL_SCREEN, fullScreenListener)
+    videoView.eventEmitter.on(EventType.DID_EXIT_FULL_SCREEN, fullScreenListener)
+  }
+
+  override fun onOrientationChanged(orientation: Int) {
+    if (
+      epsilonCheck(orientation, leftLandscape, sensorEpsilon) ||
+      epsilonCheck(orientation, rightLandscape, sensorEpsilon)
+    ) {
+      activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+      sensorEnabled = false
+      this.disable()
     }
   }
+
+  fun onNewConfigOrientation(orientation: Int) {
+    if (fullscreenTriggered) {
+      fullscreenTriggered = false
+    } else {
+      fullscreenListenerEnabled = false
+      when (orientation) {
+        Configuration.ORIENTATION_PORTRAIT -> videoView.eventEmitter.emit(EventType.EXIT_FULL_SCREEN)
+        Configuration.ORIENTATION_LANDSCAPE -> videoView.eventEmitter.emit(EventType.ENTER_FULL_SCREEN)
+      }
+    }
+  }
+
+  private fun setOppositeOrientation() {
+    val rotation = (activity.getSystemService(Context.WINDOW_SERVICE) as WindowManager)
+      .defaultDisplay
+      .rotation
+    activity.requestedOrientation = when (rotation) {
+      Surface.ROTATION_0, Surface.ROTATION_180 -> ActivityInfo.SCREEN_ORIENTATION_USER_LANDSCAPE
+      Surface.ROTATION_90, Surface.ROTATION_270 -> ActivityInfo.SCREEN_ORIENTATION_USER_PORTRAIT
+      else -> throw IllegalArgumentException("rotation is not supported")
+    }
+    sensorEnabled = true
+    this.enable()
+  }
+
+  private fun epsilonCheck(a: Int, b: Int, epsilon: Int): Boolean =
+    Math.abs(a - b) < epsilon
+
+  companion object {
+    private const val leftLandscape = 90
+    private const val rightLandscape = 270
+  }
+
 }
