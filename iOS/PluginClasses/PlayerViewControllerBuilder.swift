2e2a6be4eace9b8da314e566eea3158b6f8d02cf
Alexey Faizullov <alex.fuzza@gmail.com>
2018-11-20 15:10:27 +0200

    [iOS] Replace screen mode button with cross

--- iOS/PluginClasses/PlayerViewControllerBuilder.swift
+++ iOS/PluginClasses/PlayerViewControllerBuilder.swift
@@ -25,6 +25,13 @@ class PlayerViewBuilderImp: PlayerViewBuilder {
                                                              options: options,
                                                              controlsView: controls)
         
+        switch mode {
+        case .fullscreen:
+            setupCloseButton(for: videoView, vc: vc)
+        case .inline:
+            break
+        }
+        
         return videoView
     }
     
@@ -55,12 +62,33 @@ class PlayerViewBuilderImp: PlayerViewBuilder {
         
         switch mode {
         case .fullscreen:
-            let button: BCOVPUIButton = view.screenModeButton
-            button.setTitle(button.secondaryTitle, for: .normal)
-            button.removeTarget(nil, action: nil, for: .allTouchEvents)
-            button.addTarget(vc, action: #selector(PlayerViewController.close), for: .touchUpInside)
+            // Since we need to hide screen mode button, finding the layout view that contains screenModeButton
+            guard let items = view.layout.allLayoutItems as? Set<BCOVPUILayoutView> else { return }
+            items.first { $0.subviews.contains(view.screenModeButton) }
+                .flatMap { $0.isRemoved = true }
+            view.setNeedsLayout()            
         case .inline:
             break
         }
+    }
+    
+    // MARK: -- Private
+    
+    private func setupCloseButton(for view: BCOVPUIPlayerView, vc: PlayerViewController) {
+        let button = UIButton.blurredRoundedButton()
+        
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 24)
+        button.setTitle("\u{2573}", for: .normal)
+        button.tintColor = .white
+        button.addTarget(vc, action: #selector(PlayerViewController.close), for: .touchUpInside)
+        
+        let controls = view.controlsFadingView!
+        controls.addSubview(button)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.leftAnchor.constraint(equalTo: controls.leftAnchor, constant: 12).isActive = true
+        button.topAnchor.constraint(equalTo: controls.topAnchor, constant: 12).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44).isActive = true
     }
 }
