e5798569643230af26d310a8d6d0627ca288186f
Roman Karpievich <karpievich@mail.scand>
2019-02-01 14:04:34 +0300

    Update sample project.

--- iOS/BrightcovePlayer/InlineViewController.swift
+++ iOS/BrightcovePlayer/InlineViewController.swift
@@ -59,9 +59,8 @@ class InlineViewController: UIViewController {
         let fourthAd = ["ad_url": "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator=",
                         "offset": "90"]
         let ads = [firstAd, secondAd, thirdAd, fourthAd]
-        let advDictionary = ["video_ad": ads]
         let extensionsDictionary = ["free": "true",
-                                    "video_ads": advDictionary] as [String : Any]
+                                    "video_ads": ads] as [String : Any]
         item.extensionsDictionary = extensionsDictionary
         
         return item
