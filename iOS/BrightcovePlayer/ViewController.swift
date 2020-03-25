bb2f96f023e5af9929f028f09afe9ed914b99a4e
Roman Karpievich <karpievich@scand.com>
2020-03-25 18:53:21 +0300

    [iOS] Update sample app

--- iOS/BrightcovePlayer/ViewController.swift
+++ iOS/BrightcovePlayer/ViewController.swift
@@ -37,7 +37,7 @@ class ViewController: UIViewController {
         
         zappPlayer = BrightcovePlayerPlugin.pluggablePlayerInit(playableItems: videoItems,
                                                                 configurationJSON: nil)
-        zappPlayer?.presentPlayerFullScreen(self.tabBarController!, configuration: nil)
+        zappPlayer?.presentPlayerFullScreen(self, configuration: nil)
     }
     
     private func createVASTVideo() -> ZPPlayable {
